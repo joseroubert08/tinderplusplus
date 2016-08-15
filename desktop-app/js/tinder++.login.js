0a7cf59e4969c89ceb7836d28255b95e34ce6035
Kyle Powers <kylepowers@gmail.com>
2016-08-14 19:41:36 -0700

    new facebook oauth flow

--- desktop-app/js/tinder++.login.js
+++ desktop-app/js/tinder++.login.js
@@ -3,7 +3,7 @@
   module = angular.module('tinder++.login', ['tinder++.api']);
 
   module.controller('LoginController', function LoginController($scope, $http, API) {
-    $scope.loginUrl = 'https://m.facebook.com/dialog/oauth?client_id=464891386855067&redirect_uri=https://www.facebook.com/connect/login_success.html&scope=user_birthday,user_relationship_details,user_likes,user_activities,user_education_history,user_photos,user_friends,user_about_me,email,public_profile&response_type=token';
+    $scope.loginUrl = 'https://www.facebook.com/v2.6/dialog/oauth?redirect_uri=fb464891386855067%3A%2F%2Fauthorize%2F&state=%7B%22challenge%22%3A%22q1WMwhvSfbWHvd8xz5PT6lk6eoA%253D%22%2C%220_auth_logger_id%22%3A%2254783C22-558A-4E54-A1EE-BB9E357CC11F%22%2C%22com.facebook.sdk_client_state%22%3Atrue%2C%223_method%22%3A%22sfvc_auth%22%7D&scope=user_birthday%2Cuser_photos%2Cuser_education_history%2Cemail%2Cuser_relationship_details%2Cuser_friends%2Cuser_work_history%2Cuser_likes&response_type=token%2Csigned_request&default_audience=friends&return_scopes=true&auth_type=rerequest&client_id=464891386855067&ret=login&sdk=ios&logger_id=54783C22-558A-4E54-A1EE-BB9E357CC11F#_=';
     $scope.fbAuthData = {};
 
     $scope.startLogin = function() {
@@ -36,13 +36,12 @@
         window.clearInterval(interval);
       } else {
         var url = loginWindow.document.URL;
-        var paramString = url.split("#")[1];
-        if (!!paramString) {
-          var allParam = paramString.split("&");
-          for (var i = 0; i < allParam.length; i++) {
-            var param = allParam[i].split("=");
-            $scope.fbAuthData[param[0]] = param[1];
-          }
+        if (url === 'https://m.facebook.com/v2.6/dialog/oauth/confirm') {
+          var resp = unescape(loginWindow.document.getElementsByTagName('script')[0].innerHTML);
+          var pattern = /access_token=(.*)&/;
+          var token = resp.match(pattern)[1];
+          $scope.fbAuthData['access_token'] = token;
+
           loginWindow.close();
           window.clearInterval(interval);
           getFBUserId($scope.fbAuthData['access_token']);
